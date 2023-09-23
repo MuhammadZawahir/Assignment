3f299b2d31b4997eb9d847ee36f161570846b5b2
Muhammad Zawahir <zawahir300@gmail.com>
2023-09-24 00:00:14 +0500

    First Commit

---
+++ Exercise 3/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let myName = "Zawahir";
+let upperCaseName = myName.toUpperCase();
+console.log(`${upperCaseName}`);
+let lowerCaseName = myName.toLowerCase();
+console.log(`${lowerCaseName}`);
